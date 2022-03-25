61515ffbd6621b5f6d12431697ffd025f0bb1e34
volhashax <volhashax@gmail.com>
2022-03-25 22:14:36 +0300

    add solution

---
+++ CSharpCourse/Program.cs
@@ -0,0 +1,266 @@
+﻿using System;
+
+namespace CSharpCourse
+{
+    internal class Program
+    {
+
+        static void Main(string[] args)
+        {
+
+            string name = "abracadabra";
+
+            bool containsA = name.Contains('a');
+            bool containsE = name.Contains('E');
+
+            Console.WriteLine(containsA);
+            Console.WriteLine(containsE);
+
+            string abc = string.Concat("a", "b", "c");
+            Console.WriteLine(abc);
+
+            Console.WriteLine(int.MinValue);
+
+            int x = 1;
+            string xStr = x.ToString();
+            Console.WriteLine(xStr);
+            Console.WriteLine(x);
+
+
+
+
+        }
+
+        static void ComparisonOperators()
+        {
+            int x = 1;
+            int y = 2;
+
+            bool areEqual = x == y;
+            Console.WriteLine(areEqual);
+
+            bool result = x > y;
+            Console.WriteLine(result);
+
+            result = x >= y;
+            Console.WriteLine(result);
+
+            result = x < y;
+            Console.WriteLine(result);
+
+            result = x <= y;
+            Console.WriteLine(result);
+
+            result = x != y;
+            Console.WriteLine(result);
+        }
+
+        static void MathOperations()
+        {
+            int x = 1;
+            int y = 2;
+
+            int z = x + y;
+            int k = x - y;
+            int a = z + k - y;
+
+            Console.WriteLine(z);
+            Console.WriteLine(k);
+            Console.WriteLine(a);
+
+            int b = z * 2;
+            int c = k / 2;
+
+            Console.WriteLine(b);
+            Console.WriteLine(c);
+
+            a = 4 % 2;
+            b = 5 % 2;
+
+            Console.WriteLine(a);
+            Console.WriteLine(b);
+
+            a = 3;
+            a = a * a;
+            // a = a * a * a;
+
+            Console.WriteLine(a);
+
+            a = 2 + 2 * 2;
+
+            Console.WriteLine(a);
+
+            a *= 2;
+
+            Console.WriteLine(a);
+
+            a /= 2;
+            //a=a/2;
+            Console.WriteLine(a);
+        }
+
+        static void IncrementDecrementDemo()
+        {
+            int x = 1;
+
+            x = x + 1;
+            Console.WriteLine(x);
+
+            x++;
+
+            Console.WriteLine(x);
+
+            ++x;
+            Console.WriteLine(x);
+
+            x = x - 1;
+
+            x--;
+            --x;
+            Console.WriteLine(x);
+
+            Console.WriteLine("Learn about increments");
+            Console.WriteLine($"Last x state is {x}");
+
+            int j = x++;
+
+            Console.WriteLine(j);
+            Console.WriteLine(x);
+
+
+            j = ++x;
+
+            Console.WriteLine(j);
+            Console.WriteLine(x);
+
+            x += 2;
+            //x = x +2;
+            Console.WriteLine(x);
+
+            j -= 2;
+            //j=j-2;
+            Console.WriteLine(j);
+        }
+
+
+        static void Overflow()
+        {
+            checked
+            {
+
+
+                uint x = uint.MaxValue;
+
+                Console.WriteLine(x);
+
+                x = x + 1;
+
+                Console.WriteLine(x);
+
+                x = x - 1;
+
+                Console.WriteLine(x);
+
+            }
+        }
+
+        static void VariablesScope()
+        {
+            var a = 1;
+            {
+                var b = 2;
+                {
+                    var c = 3;
+
+                    Console.WriteLine(a);
+                    Console.WriteLine(b);
+                    Console.WriteLine(c);
+                }
+
+                Console.WriteLine(a);
+                Console.WriteLine(b);
+                // Console.WriteLine(c);
+            }
+            Console.WriteLine(a);
+            //Console.WriteLine(b);
+            // Console.WriteLine(c);
+        }
+
+        static void Literals()
+        {
+            int x = 0b11;
+            int y = 0b1001;
+            int k = 0b10001001;
+            int m = 0b1000_1001;
+
+            Console.WriteLine(x);
+            Console.WriteLine(y);
+            Console.WriteLine(k);
+            Console.WriteLine(m);
+
+            x = 0x1f;
+            y = 0xFF0D;
+            k = 0x1FAB30EF;
+            m = 0x1FAB_30EF;
+
+            Console.WriteLine();
+
+            Console.WriteLine(x);
+            Console.WriteLine(y);
+            Console.WriteLine(k);
+            Console.WriteLine(m);
+
+            Console.WriteLine();
+
+            Console.WriteLine(4.5e2);
+            Console.WriteLine(3.1E-1);
+
+            Console.WriteLine();
+
+            Console.WriteLine('\x78');
+            Console.WriteLine('\x5A');
+
+            Console.WriteLine('\u0420');
+            Console.WriteLine('\u0421');
+        }
+
+        static void Variables()
+        {
+            int x = -1;
+
+            int y;
+            y = 2;
+
+            //Int32 x1 = -1;
+            //unit z = -1;
+
+            float f = 1.1f;
+            double d = 2.3;
+
+            int x2 = 0;
+            int x3 = new int();
+
+            var a = 1;
+            var b = 1.2;
+
+            //Dictionary<int, string> dict = new Dictionay,int, string>();
+            //var dict = new Dictionary<int, string>();
+
+            //var v;
+
+            decimal money = 3.0m;
+
+            char @char = 'A';
+            string name = "John";
+
+            bool canDrive = true;
+            bool canDraw = false;
+
+            object obj1 = 1;
+            object obj2 = "obj2";
+
+            Console.WriteLine(a);
+            Console.WriteLine(name);
+        }
+    }
+}

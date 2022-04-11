11b27d9ebcb410026c4c760d52b24317708ef707
volhashax <volhashax@gmail.com>
2022-04-11 23:01:43 +0300

    inheritance

--- D_OOP/Character.cs
+++ D_OOP/Character.cs
@@ -6,9 +6,32 @@ namespace D_OOP
 {
     public class Character
     {
-        private const int speed = 10;
+        private const int speed;
         public int Health { get; private set; } = 100;
 
+        public string Race { get; private set; }
+
+        public int Armor { get; private set; }
+
+        public Character(string race)
+        {
+            Race = race;
+            Armor = 30;
+        }
+
+
+        public Character(string race, int armor)
+        {
+            Race = race;
+            Armor = armor;
+        }
+
+        //public Character(string race, int armor, int speed)
+        //{
+        //    Race = race;
+        //    Armor = armor;
+        //    this.speed = speed;
+        //}
         public void Hit(int damage)
         {
             if (damage > Health)
